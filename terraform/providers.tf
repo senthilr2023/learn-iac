5e77c4c981ebff8e796c0ac08bf0a716004e1358
Senthil Nathan Ramadoss <senthilr01@gmail.com>
2024-03-26 23:33:04 +0100

    initial infra

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,26 +9,19 @@ terraform {
       version = "~>3.0"
     }
   }
+  #the key should ideally be placed in a keyvault or in a environment variable so that it is not written in the disk
   backend "azurerm" {
-      resource_group_name  = "tfbasics"
-      storage_account_name = "<storage_account_name>"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
+    resource_group_name  = "tfbasics"
+    storage_account_name = "stbasicstf5673"
+    container_name       = "tfstate"
+    key                  = "***"
   }
-
 }
 
 provider "azurerm" {
-  
-  # ServicePrinciple can also be used
-  # subscription_id   = "<azure_subscription_id>"
-  # tenant_id         = "<azure_subscription_tenant_id>"
-  # client_id         = "<service_principal_appid>"
-  # client_secret     = "<service_principal_password>"
   features {}
 }
 
-resource "azurerm_resource_group" "state-demo-secure" {
-  name     = "state-demo"
-  location = "eastus"
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
